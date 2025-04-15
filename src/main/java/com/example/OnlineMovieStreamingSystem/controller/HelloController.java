5425c5ea35052dee3073fdfe05927aba565ab355
QuyHoang7703 <hoangvanquy772003@gmail.com>
2025-04-15 15:07:23 +0700

    add handler for authorization, no resource exception

--- src/main/java/com/example/OnlineMovieStreamingSystem/controller/HelloController.java
+++ src/main/java/com/example/OnlineMovieStreamingSystem/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.example.OnlineMovieStreamingSystem.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
+    @PreAuthorize("hasRole('ADMIN')")
     public String helloWorld() {
         return "Hello World";
     }
--- src/main/java/com/example/OnlineMovieStreamingSystem/util/exception/GlobalException.java
+++ src/main/java/com/example/OnlineMovieStreamingSystem/util/exception/GlobalException.java
@@ -4,12 +4,15 @@ import com.example.OnlineMovieStreamingSystem.domain.RestResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
@@ -52,5 +55,23 @@ public class GlobalException {
         restResponse.setMessage(errors.size() > 1 ? errors : errors.get(0));
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(restResponse);
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<RestResponse<Object>> handleAccessDeniedException(AuthorizationDeniedException ex) {
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(HttpStatus.FORBIDDEN.value());
+        res.setMessage(ex.getMessage());
+        res.setError("Access Denied");
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(res);
+    }
+
+    @ExceptionHandler(value={NoResourceFoundException.class, NoHandlerFoundException.class})
+    public ResponseEntity<Object> handleNotFoundException(Exception ex) {
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(HttpStatus.NOT_FOUND.value());
+        res.setError(ex.getMessage());
+        res.setMessage("404 Not Found. Url may not exist...");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(res);
     }
 }
