035822aa73741fd5e36c5bfd9310a61db1209eb2
QuyHoang7703 <hoangvanquy772003@gmail.com>
2025-06-16 07:42:54 +0700

    user-interaction

--- src/main/java/com/example/OnlineMovieStreamingSystem/repository/UserInteractionRepository.java
+++ src/main/java/com/example/OnlineMovieStreamingSystem/repository/UserInteractionRepository.java
@@ -1,6 +1,8 @@
 package com.example.OnlineMovieStreamingSystem.repository;
 
 import com.example.OnlineMovieStreamingSystem.domain.UserInteraction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,5 +16,9 @@ public interface UserInteractionRepository extends JpaRepository<UserInteraction
     long countRatingsOfUser(@Param("userId") long userId);
 
     Optional<UserInteraction> findByUserIdAndMovieId(long userId, long movieId);
+
+    @Query("SELECT ui FROM UserInteraction ui " +
+            "WHERE ui.userTemporaryId = :userId")
+    Page<UserInteraction> findByUserId(@Param("userId") Long userId, Pageable pageable);
 
 }
